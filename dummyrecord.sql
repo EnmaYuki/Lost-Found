89a673060f3fc914575bb2f23597f4c7dd780466
Elayne <elaynelam@gmail.com>
2024-02-06 20:19:24 +0800

    reupdate the structure of database
    add user into target_user table first
    then insert the username and password into user table as backup

--- dummyrecord.sql
+++ dummyrecord.sql
@@ -27,80 +27,50 @@ USE `lost&found_db`;
 -- --------------------------------------------------------
 
 --
--- Table structure for table `admin`
---
-
-DROP TABLE IF EXISTS `admin`;
-CREATE TABLE IF NOT EXISTS `admin` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `adminID` varchar(8) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'ADMIN',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `adminID` (`adminID`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `admin`
---
-
-INSERT INTO `admin` (`id`, `adminID`) VALUES
-(1, 'ADMIN');
-
--- --------------------------------------------------------
-
-
---
--- Table structure for table `user`
+-- Table structure for table `user` for store all users including admin
 --
 
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE IF NOT EXISTS `user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `userid` varchar(8) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `uid` int(8) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `admin` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`uid`)
+  
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+INSERT INTO `user` (`uid`, `username`,`password`,`admin`) VALUES
+(00000001, 'ADMIN','administration',1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `target_user` for user who is not admin
+--
+DROP TABLE IF EXISTS `target_user`;
+CREATE TABLE IF NOT EXISTS `target_user` (
+  `id` int(8) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT,
   `username` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
   `password` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
   `email` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
   `mobile` varchar(8) COLLATE utf8mb4_unicode_ci NOT NULL,
   `nickname` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `VIP` tinyint(1) NOT NULL DEFAULT '0'
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `userid` (`userid`)
+  `VIP` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
---
--- Dumping data for table `user`
---
-/*
-INSERT INTO `user` (`id`, `userid`, `username`, `password`, `email`, `mobile`, `nickname`, `VIP`) VALUES
-(1, 'ADMIN', 'admin', 'administrator', 'admin@mail.com', '24761234', 'admin', 1);
-*/
+
 
 DROP TABLE IF EXISTS `item`;
 CREATE TABLE IF NOT EXISTS `item` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `itemid` varchar(5) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `uid` varchar(8) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `district_id` varchar(2) COLLATE utf8mb4_unicode_ci  NOT NULL,
-  `found` int(1) NOT NULL,
-  PRIMARY KEY (`id`)
+  `item_id` int(5) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT,
+  `uid` int(8) UNSIGNED ZEROFILL NOT NULL,
+  `description` varchar(99) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `district_id` int(2) UNSIGNED ZEROFILL NOT NULL,
+  `found` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`item_id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `item`
---
-
-INSERT INTO `item` (`id`, `itemid`, `uid`, `district_id`, `found`) VALUES
-(1, 'TP202', '54389231', '06', 0),
-(2, 'YL202', '12239222', '09', 0),
-(3, 'NC202', '12223444', '11', 0),
-(4, 'TM103', '12223333', '08', 0),
-(5, 'MK354', '13332333', '14', 0),
-(6, 'TST32', '12322443', '13', 0),
-(7, 'TW342', '12323442', '15', 0),
-(8, 'TP342', '12663474', '17', 1),
-(9, 'TST45', '12454355', '01', 1),
-(10, 'ST453', '12546533', '02', 1),
-(11, 'TM453', '17653774', '05', 0),
-(12, 'YL002', '13554464', '18', 0);
 
 -- --------------------------------------------------------
 
@@ -110,36 +80,35 @@ INSERT INTO `item` (`id`, `itemid`, `uid`, `district_id`, `found`) VALUES
 
 DROP TABLE IF EXISTS `HK_districts`;
 CREATE TABLE IF NOT EXISTS `HK_districts` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `district_id` varchar(2) COLLATE utf8mb4_unicode_ci NOT NULL,
+ `district_id` int(2) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT,
   `district_name` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
   `region_id` int(1) COLLATE utf8mb4_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`district_id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `HK_districts`
 --
 
-INSERT INTO `HK_districts` (`id`,`district_id` , `district_name`, `region_id`) VALUES
-(1,'01', 'Islands', 1),
-(2,'02', 'Kwai Tsing', 1),
-(3,'03', 'North', 1),
-(4,'04', 'Sai Kung', 1),
-(5,'05', 'Sha Tin', 1),
-(6,'06', 'Tai Po', 1),
-(7,'07', 'Tsuen Wan', 1),
-(8,'08', 'Tuen Mun', 1),
-(9,'09', 'Yuen Long', 1),
-(10,'10', 'Kowloon City', 2),
-(11,'11', 'Kwun Tong', 2),
-(12,'12', 'Sham Shui Po', 2),
-(13,'13', 'Wong Tai Sin', 2),
-(14,'14', 'Yau Tsim Mong', 2),
-(15,'15', 'Central and Western', 3),
-(16,'16', 'Eastern', 3),
-(17,'17', 'Southern', 3),
-(18,'18', 'Wan Chai', 3);
+INSERT INTO `HK_districts` (`district_id` , `district_name`, `region_id`) VALUES
+(01, 'Islands', 1),
+(02, 'Kwai Tsing', 1),
+(03, 'North', 1),
+(04, 'Sai Kung', 1),
+(05, 'Sha Tin', 1),
+(06, 'Tai Po', 1),
+(07, 'Tsuen Wan', 1),
+(08, 'Tuen Mun', 1),
+(09, 'Yuen Long', 1),
+(10, 'Kowloon City', 2),
+(11, 'Kwun Tong', 2),
+(12, 'Sham Shui Po', 2),
+(13, 'Wong Tai Sin', 2),
+(14, 'Yau Tsim Mong', 2),
+(15, 'Central and Western', 3),
+(16, 'Eastern', 3),
+(17, 'Southern', 3),
+(18, 'Wan Chai', 3);
 
 
 -- --------------------------------------------------------
@@ -154,7 +123,7 @@ CREATE TABLE IF NOT EXISTS `HK_region` (
   `region_id` int(1) NOT NULL,
   `name` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`region_id`),
-  UNIQUE KEY `r_id` (`region_id`)
+  UNIQUE KEY `region_id` (`region_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
